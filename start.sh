51b33259368b6c1dfcf2f5a829a2c868a1861bbe
zsimpson-mousera <zack@vium.com>
2017-08-28 14:47:00 -0700

    start stop

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo nohup ./fira_server.py >fira.log 2>&1 &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 sudo kill $(ps aux | grep 'fira' | awk '{print $2}')
-sudo nohup ./fira_server.py >fira_server.log 2>&1 &
