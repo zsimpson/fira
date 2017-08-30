68aa1f70a50fa0cb8c2cb097e6e5c42c283b47de
zsimpson-mousera <zack@vium.com>
2017-08-30 11:23:27 -0700

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,4 @@ $SSH sudo kill -9 $PID
 PID=$($SSH ps ax | grep 'fira_server.py' | awk '{print $1}')
 $SSH sudo kill -9 $PID
 
-#$SSH "cd /home/ubuntu/fira/; git pull origin master; sudo nohup ./fira_server.py >fira.log 2>&1 &"
-$SSH "cd /home/ubuntu/fira/; git pull origin master"
+$SSH "cd /home/ubuntu/fira/; git pull origin master; sudo nohup ./fira_server.py >fira.log 2>&1 &"
