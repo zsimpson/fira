ea02fa9a82bf804dc3750b105deaf8e77dc695c0
zsimpson-mousera <zack@vium.com>
2017-09-02 21:36:09 -0700

    deploy

--- fira_server.py
+++ fira_server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -u
+#!/usr/bin/env python
 import httplib
 import sys
 import urllib
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source secret.sh
-sudo -E nohup ./fira_server.py >fira.log 2>&1 &
+sudo -E nohup python -u ./fira_server.py >fira.log 2>&1 &
 echo "Started $?"
