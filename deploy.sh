4ef263ba476c437babb2a9375c9fc09d7700729f
zsimpson-mousera <zack@vium.com>
2017-09-02 21:10:07 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git add .
 git commit -m "deploy"
 git push origin master
 
+
 SSH="ssh -i ~/vium/keys/prod_ecs_host.pem ubuntu@10.204.1.139" 
 
 PID=$($SSH ps ax | grep 'nohup ./fira_server.py' | awk '{print $1}')
@@ -12,4 +13,5 @@ $SSH sudo kill -9 $PID
 PID=$($SSH ps ax | grep 'fira_server.py' | awk '{print $1}')
 $SSH sudo kill -9 $PID
 
-$SSH "cd /home/ubuntu/fira/; git pull origin master; sudo nohup ./fira_server.py >fira.log 2>&1 &"
+$SSH "[ -z \"\$GITHUB_SECRET\" ] && echo \"Need to set GITHUB_SECRET\" && exit 1"
+$SSH "cd /home/ubuntu/fira/; printenv > env; git pull origin master; sudo -E nohup ./fira_server.py >fira.log 2>&1 &"
